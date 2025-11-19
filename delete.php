bcd68f0630c537181eab40e48f4b398a4f31e1dd
Daffa Yusuf Mahendra <daffalegendaris@gmail.com>
2025-11-19 11:35:31 +0700

    second commit
    Add validation and prepared statement for delete operation

--- delete.php
+++ delete.php
@@ -1,7 +1,19 @@
 <?php
 include 'db.php';
-$id = $_GET['id'];
 
-mysqli_query($connection, "DELETE FROM mahasiswa WHERE id=$id");
+// Validasi ID hanya angka
+$id = intval($_GET['id']);
+
+if ($id <= 0) {
+    die("ID tidak valid");
+}
+
+// Gunakan prepared statement
+$stmt = mysqli_prepare($connection, "DELETE FROM mahasiswa WHERE id = ?");
+mysqli_stmt_bind_param($stmt, "i", $id);
+mysqli_stmt_execute($stmt);
+
+// Redirect kembali
 header("Location: index.php");
+exit;
 ?>
